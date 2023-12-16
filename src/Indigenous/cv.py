4232dbdaf5baae98031a486a0f5e461c9532bec5
diego <diegopgs@gmali.com>
2023-12-16 14:52:05 -0300

    cv code

--- src/Indigenous/cv.py
+++ src/Indigenous/cv.py
@@ -1,4 +1,5 @@
 import os
+import shutil
 
 def getSentences(conllu_file):
   sentences = []
@@ -15,20 +16,18 @@ def getSentences(conllu_file):
 
 def CV(sentences):
 
-  test_folder = []
-  train_folder = []
+  folders_split = ['' for i in range(5)]
+  folders = [{'train':'','test':''} for i in range(5)]
 
-  size_sentences = len(sentences) 
-  size_folder = size_sentences//5
+ 
+  for index in range(len(sentences)):
+    folders_split[index % 5] += '\n'.join(sentences[index])
 
-  for index in range(0, len(sentences),size_folder):
+  for index in range(5):
+    folders[index]['test']  = folders_split[index]
+    folders[index]['train'] = ''.join(folders_split[0:index])+''.join(folders_split[index+1:])
 
-    test_folder.append('\n'.join(sentences[index:index+size_folder]))
-    train_folder.append('\n'.join(sentences[index+size_folder:]))
-    train_folder[-1] += '\n'.join(sentences[0:index])
-
-  return test_folder,train_folder
-
+  return folders
 
 def getCVFolders(PATH_CONLLU):
   languages_CV = {}
@@ -36,8 +35,42 @@ def getCVFolders(PATH_CONLLU):
   for language in os.listdir(PATH_CONLLU):
     PATH = '%s/%s' % (PATH_CONLLU,language)
 
+    
     for arquivo in os.listdir(PATH):
-      test, train = CV(getSentences(PATH+'/'+arquivo))
-      languages_CV[language] = {'test':test,'train':train}
-
+        folders = CV(getSentences(PATH+'/'+arquivo))
+        languages_CV[language] = {'test':[f['test'] for f in folders],'train':[f['train'] for f in folders]}
+  
   return languages_CV
+
+
+def createFolders(PATH,folders):
+
+  try:
+    os.mkdir(PATH)
+  except:
+    print(PATH,' Already exist. All files within folder will be deleted')
+    shutil.rmtree(PATH)
+    os.mkdir(PATH)
+
+
+  for language, data in folders.items():
+    os.mkdir('%s/%s' % (PATH,language))
+    for index,conllu in enumerate(data['train']):
+        file_name_train = '%s__%s__train.conllu' % (language,index)
+        writerTR = open('%s/%s/%s' % (PATH,language,file_name_train),'w')
+        writerTR.write(conllu)
+
+    for index,conllu in enumerate(data['test']):
+        file_name_train = '%s__%s__test.conllu' % (language,index)
+        writerTR = open('%s/%s/%s' % (PATH,language,file_name_train),'w')
+        writerTR.write(conllu)
+  
+
+  # if language not in os.listdir(PATH):
+  #   os.mkdir(language)
+
+  # writer_test = open('%s/%s_test_.conllu')
+    
+if "__main__":
+  folders = getCVFolders('/home/diego/ProjectGit/DG_MI_model/data/')  
+  createFolders('/home/diego/ProjectGit/DG_MI_model/data_cv/',folders)
